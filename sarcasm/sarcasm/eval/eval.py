ed005c4c6356b542654614096462ba8b455d1b63
centerrocket <142695150+centerrocket@users.noreply.github.com>
2023-12-07 05:54:40 +0800

    Update eval.py
    eval.py from HW4

--- sarcasm/sarcasm/eval/eval.py
+++ sarcasm/sarcasm/eval/eval.py
@@ -1,38 +1,16 @@
 from sklearn.metrics import precision_score, recall_score, f1_score
-from sarcasm.sarcasm_model import Logistic
-from sarcasm.sarcasm_model import NaiveBayes
 
+def evaluate(results, metric):
 
-def Logistic_Eval(X_train, y_train, X_test, y_test,metric):
-    """
-    Evaluate the results for Logistic Regression model.
-    """
-    sarcasm_model = Logistic()
-    tfidf, LogisticModel = sarcasm_model.train(X_train,y_train)
-    y_pred = sarcasm_model.test(LogisticModel,tfidf,X_test)
+    y_true = [x[2] for x in results] 
+    y_pred = [x[0] == x[1] for x in results]
 
     if metric == 'precision':
-        return precision_score(y_test, y_pred, average='binary')
+        return precision_score(y_true, y_pred)
     elif metric == 'recall':
-        return recall_score(y_test, y_pred, average='binary')
+        return recall_score(y_true, y_pred)
     elif metric == 'f1':
-        return f1_score(y_test, y_pred, average='binary')
+        return f1_score(y_true, y_pred)
     else:
-        raise ValueError("Unknown metric: {}".format(metric))
-    
-def NaiveBayes_Eval(X_train, y_train, X_test, y_test,metric):
-    """
-    Evaluate the results for Naive Bayes model.
-    """
-    sarcasm_model = NaiveBayes(var_smoothing=0.1)
-    vectorizer,classifier= sarcasm_model.train(X_train,y_train)
-    y_pred = sarcasm_model.test(classifier,vectorizer,X_test)
+        raise ValueError("Unsupported metric. Choose 'precision', 'recall', or 'f1'.")
 
-    if metric == 'precision':
-        return precision_score(y_test, y_pred, average='binary')
-    elif metric == 'recall':
-        return recall_score(y_test, y_pred, average='binary')
-    elif metric == 'f1':
-        return f1_score(y_test, y_pred, average='binary')
-    else:
-        raise ValueError("Unknown metric: {}".format(metric))
